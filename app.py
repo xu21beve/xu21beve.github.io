b1567e78115d9cae2790352a221daea5120484ab
xu21beve <73717685+xu21beve@users.noreply.github.com>
2024-04-07 00:36:10 -0700

    set app.py debug=False

--- app.py
+++ app.py
@@ -23,5 +23,5 @@ def upload():
     return send_file(audio_path, as_attachment=False)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
 
